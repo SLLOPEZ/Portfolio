50210a7c66f95c7b23366e351e2de6995f6b2f6f
Stephanie Lopez <lopezlstephanie@gmail.com>
2016-03-28 16:02:52 -0700

    added color scheme to page and a scroll function

--- scripts/views.js
+++ scripts/views.js
@@ -5,8 +5,17 @@ contentView.handleMainNav = function() {
     $('.tab-content').hide();
     $('#' + $(this).data('content')).fadeIn();
   });
-  $('#nav-menu .tab:first').click();
+  // $('#nav-menu .tab:first').click();
 };
+contentView.scrollEffect = function () {
+  $('#nav-menu').on('click', 'li', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+      'scrollTop': $('#' + $(this).data('content')).offset().top});
+  });
+};
+
 $(document).ready(function () {
   $(contentView.handleMainNav());
+  $(contentView.scrollEffect());
 });
